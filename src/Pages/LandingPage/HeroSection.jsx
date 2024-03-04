7c5632122ef580f5d865058dbab7cf618276e387
RatulRahmanRudra <ratul.rahman.rrr@gmail.com>
2024-03-05 02:21:54 +0600

    updated landing text

--- src/Pages/LandingPage/HeroSection.jsx
+++ src/Pages/LandingPage/HeroSection.jsx
@@ -7,11 +7,12 @@ const HeroSection = () => {
             <div class="px-4 mx-auto mt-16 mb-16 max-w-7xl sm:mt-24">
                 <div class="text-center">
                     <h1 class="text-4xl font-extrabold tracking-tight text-gray-200 sm:text-5xl md:text-6xl font-title">
-                        <span class="block">International Conference on Nanoscience and Nanotechnology</span>
+                        <span class="block">2nd International Conference on Textile Science and Engineering</span>
                         {/* <span class="block pt-2">New Product Launch</span> */}
                     </h1>
+                    <h2 className="mt-2 text-white">With special focus on Textile Processing, Sustainability and Circularity</h2>
                     <p class="max-w-md mx-auto mt-3 text-base text-gray-300 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-                        13-15 FEBRUARY 2024 | MELBOURNE CONVENTION & EXHIBITION CENTRE
+                    23-24 May 2024, BUTEX, Dhaka, Bangladesh
                     </p>
                     <div class="max-w-md mx-auto mt-5 sm:flex sm:justify-center md:mt-8">
 
