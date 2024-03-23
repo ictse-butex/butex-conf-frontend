bd61bd2f6e00cee4b24df0a60e3e44e6bad38cf5
RatulRahmanRudra <ratul.rahman.rrr@gmail.com>
2024-03-23 06:06:26 +0600

    updated for cpanel hosting

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -8,6 +9,7 @@ export default defineConfig(({ mode }) => {
     define: {
       'process.env.API_URL': JSON.stringify(env.API_URL)
     },
-    plugins: [react()],
+    plugins: [react(), viteSingleFile()],
+    base: "https://www.ictsebutex.com/"
   }
 })
