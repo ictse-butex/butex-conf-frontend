21ab742434f69405346a850ff77dfbfc40e4b791
RatulRahmanRudra <ratul.rahman.rrr@gmail.com>
2024-05-22 18:26:40 +0600

    speakers added

--- src/Pages/NavbarPages/Speakers.jsx
+++ src/Pages/NavbarPages/Speakers.jsx
@@ -1,22 +1,76 @@
-import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import React, { useEffect, useState, } from 'react';
+import { pdfjs, Document, Page } from 'react-pdf';
+import Modal from 'react-modal';
+import '../../style/speaker.css';
+
+// import '../../asset/speakers.pdf'
+
+ const Speaker = () => {
+
+  const generatePdfDirectory = (start, end) => {
+    const pdfDirectory = [];
+  
+    for (let i = start; i <= end; i++) {
+      // Pad the number with leading zeros to ensure it is three digits
+      const fileNumber = String(i).padStart(3, '0');
+      pdfDirectory.push(`assets/speakers-bio/${fileNumber}.pdf`);
+    }
+  
+    return pdfDirectory;
+  };
+  
+
+  const pdfDirectory = generatePdfDirectory(1, 44);
+  
+  // const pdfDirectory = [
+  //   'assets/speakers-bio/001.pdf',
+  //   'assets/speakers-bio/002.pdf',
+  //   'assets/speakers-bio/003.pdf',
+  //   'assets/speakers-bio/004.pdf',
+  // ]
+
+  const [selectedPdf, setSelectedPdf] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
 
 
-const Speakers = () => {
-    return (
-    <div className="carousel-wrapper w-4/5 max-h-fit pt-5 m-auto"  style={{
-    }}>
-        <Carousel autoPlay={true} className='h-1/2'>
-            {[...Array(34)].map((_, index) => (
-            <div key={index} className='h-1/2'>
-                <img src={`https://ictsebutex.com/assets/speakers/${index}.jpg`} />
-            </div>
-            ))}
-        </Carousel>
+  const openModal = (pdf) => {
+    setSelectedPdf(pdf);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedPdf(null);
+  };
+
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+  return (
+    <div className="pdf-grid">
+      {pdfDirectory.map((pdf, index) => (
+        <div key={index} className="pdf-item" onClick={() => openModal(pdf)}>
+          <Document file={pdf}>
+            <Page pageNumber={1} width={150} />
+          </Document>
+        </div>
+      ))}
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="PDF Modal"
+        className="pdf-modal"
+        overlayClassName="pdf-modal-overlay"
+        key={selectedPdf}
+      >
+        {selectedPdf && (
+          <Document file={selectedPdf}>
+            <Page pageNumber={1} />
+          </Document>
+        )}
+      </Modal>
     </div>
-      );
-};
+  );
+ }
 
-export default Speakers;
+ export default Speaker;
---
+++ src/data/speakers.js
@@ -0,0 +1,49 @@
+const speakers = [
+    {
+      manuscriptTitle: `Fabric of the Future: Statistical Approaches to Solar Energy Utilization in Bangladesh's Textile Realm`,
+      presentingAuthor: {
+        name: "Md. Mortuza Ahmmed",
+        affiliation: "Associate Professor, Department of Mathematics, American International University-Bangladesh (AIUB), Dhaka, Bangladesh",
+        bio: `Md. Mortuza Ahmmed a Statistician with an extensive range of research interests. His
+            interests are mainly application based -- how to apply different statistical techniques in
+            different sectors to perform more accurate and precise analyses as well as projection. His
+            core academic research areas are public health, education and machine learning. One of his
+            primary goals concerning students is to assist them with technical knowledge to turn their
+            thoughts into successful outcomes to form a modern society.`,
+        image: "https://picsum.photos/200/200"
+      },
+      correspondingAuthors: [
+        {
+          name: "Mian Mohammad Rassel",
+          affiliation: "American International University-Bangladesh (AIUB), Dhaka, Bangladesh",
+          bio: "I am Mian Mohammad Rassel, a dedicated student at American International University...",
+          image: "https://picsum.photos/200/300"
+        },
+        {
+          name: "Mian Mohammad Rassel",
+          affiliation: "American International University-Bangladesh (AIUB), Dhaka, Bangladesh",
+          bio: "I am Mian Mohammad Rassel, a dedicated student at American International University...",
+          image: "https://picsum.photos/200/300"
+        }
+    ]
+    },
+
+    {
+        presentingAuthor: {
+          name: "Md. Mortuza Ahmmed",
+          affiliation: "American International University-Bangladesh (AIUB), Dhaka, Bangladesh",
+          bio: "Md. Mortuza Ahmmed a Statistician with an extensive range of research interests...",
+          image: "https://picsum.photos/200/200"
+        },
+        correspondingAuthors: [{
+          name: "Mian Mohammad Rassel",
+          affiliation: "American International University-Bangladesh (AIUB), Dhaka, Bangladesh",
+          bio: "I am Mian Mohammad Rassel, a dedicated student at American International University...",
+          image: "https://picsum.photos/200/300"
+        }]
+      },
+
+      
+  ];
+
+  export default speakers;
